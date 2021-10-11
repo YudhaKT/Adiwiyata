5dc97bb2ca484ab18883c52e03fc1c3bfb98eada
YudhaKT <72378616+YudhaKT@users.noreply.github.com>
2021-10-11 20:44:06 +0700

    Update ListTanaman.java

--- app/src/main/java/com/example/adiwiyata_admin/ListTanaman.java
+++ app/src/main/java/com/example/adiwiyata_admin/ListTanaman.java
@@ -87,9 +87,7 @@ public class ListTanaman extends AppCompatActivity {
         });
         etSearch.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
